048a7b16d481af891f4e9f31d9ee3e1045d65002
Dmitry Kn <4dmitry.kn@gmail.com>
2021-05-14 17:25:28 -0400

    add Router and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
+import {
+  Home,
+  About,
+  Auth,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from './pages';
 
 function App() {
-  return <h4>Fancy</h4>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/products/:id' exact children={SingleProduct} />
+        <Route path='/auth' exact component={Auth} />
+        <Route path='/cart' exact component={Cart} />
+        <Route path='/checkout' exact component={Checkout} />
+        <Route path='*' component={Error} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
